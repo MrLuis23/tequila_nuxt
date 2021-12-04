6c3d42c5b2adb04ad3a894ba756c1590f58d99dd
LuisMR <mr_luis23@hotmail.com>
2021-12-04 11:22:51 -0600

    quitamos la historia

--- pages/home.vue
+++ pages/home.vue
@@ -2,8 +2,7 @@
   <v-container fluid class="pa-0 " style="background: #1a1a1a;" >
     <v-container pa-0 class="d-flex flex-column white--text  justify-space-between" width="100vw"  fluid>      
       <NavBar />
-      <MainCorousel id="top" name="top" />
-      <History id="history" name="history"></History>
+      <MainCorousel id="top" name="top" />      
       <Products  id="products" name="products" />
       <Footer/>
     </v-container>
@@ -14,14 +13,13 @@
 <script>
   import MainCorousel from '../components/MainCorousel';
   import NavBar from '@/components/NavBar';
-  import Footer from '@/components/Footer';
-  import History from '@/components/History';
+  import Footer from '@/components/Footer';  
   import Products from '@/components/Products.vue';
 
   export default {
     name: 'Home',
     components: {
-      MainCorousel, NavBar,Footer,History,Products
+      MainCorousel, NavBar,Footer,Products
     },
   }
 </script>
